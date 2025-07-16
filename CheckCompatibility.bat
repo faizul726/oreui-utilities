cf1c9d9d5d28a93203fdcd76d82a15919c42d527
faizul726 <162413089+faizul726@users.noreply.github.com>
2025-07-16 16:15:19 +0600

    add compatibility check workflow

---
+++ CheckCompatibility.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal
+
+mkdir tmp
+git worktree add tmp gh-pages
+
+echo Looking for json file...
+for %%F in ("src\hbui\*.json") do (
+    echo Found %%~nxF
+    set "currentFileName=%%~nxF"
+)
+
+echo Downloading appx...
+curl -Lo %1 mcappx.zip
+
+echo Extracting appx...
+7z x mcappx.zip -otmp
+
+echo Checking compatibility...
+if exist "tmp\data\gui\dist\hbui\%currentFileName%" (
+    echo Compatible with %2
+    >tmp\compatibility-status.json echo {"schemaVersion":1,"label":"%2","message":"Supported","color":"green"}
+    set exitCode=0
+) else (
+    echo Incompatible with %2
+    >tmp\compatibility-status.json echo {"schemaVersion":1,"label":"%2","message":"Not supported, needs update","color":"red"}
+    set exitCode=6
+)
+
+git add tmp\compatibility-status.json
+git commit -m "Compatibility check"
+git push origin gh-pages
+
+exit %exitCode%
